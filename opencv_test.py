e9ba97180e29d9dce914ec695ec9430df29f48ef
ray <rayanticlimatic@gmail.com>
2019-09-09 12:17:36 +0900

    mostly finished opencv test

--- opencv_test.py
+++ opencv_test.py
@@ -4,10 +4,16 @@ import cv2
 a = np.zeros((4,4,4), dtype="uint8")
 a[0,1,1] =a[0,3,1] =a[1,1,1] =a[1,1,0] = 1
 canvas_size = 128
-x_coor = [int(canvas_size*(1/8+1/4*i)) for i in range(4)]
-y_coor = x_coor
+coor = [int(canvas_size*(1/8+1/4*i)) for i in range(4)]
+h_margin = canvas_size*5/8
+w_margin = canvas_size*4/8
+list_srcs = np.float32([[0,0],[0,canvas_size],[canvas_size,canvas_size],[canvas_size,0]])
+h_warp = int(canvas_size-h_margin)
+w_warp = int(canvas_size+w_margin)
+stride = int(canvas_size*3/8)
 
 canvases = [np.zeros((canvas_size, canvas_size, 3), dtype="uint8") for i in range(4)]
+new_canvas = np.zeros((int(canvas_size*5/8), int(canvas_size*5/8), 3), dtype="uint8")
 def plot_circle():
     global canvases
     for i in range(4):
@@ -16,9 +22,17 @@ def plot_circle():
         if len(points[0]) == 0:
             continue 
         for x,y in zip(points[0], points[1]):
-            cv2.circle(canvas,(x_coor[x], y_coor[y]), 13, (255,0,0), -1)
+            cv2.circle(canvas,(coor[x], coor[y]), 13, (255,0,0), -1)
+
+def warp():
+    global new_canvas
+    list_dsts = np.float32([[0+h_margin, 0+w_margin],list_srcs[1],list_srcs[2],[canvas_size+h_margin, 0+w_margin]])
+    perspective_matrix = cv2.getPerspectiveTransform(list_srcs, list_dsts)
+    for count, canvas in enumerate(canvases):
+        dst = cv2.warpPerspective(canvas, perspective_matrix, (h_warp, w_warp))
+        new_canvas[stride*count:stride*count + h_warp,:,:] += dst*0.5
 
 plot_circle()
-for count, canvas in enumerate(canvases):
-    cv2.imshow(f"test {count}", canvas)
-    cv2.waitKey(1000)
+warp()
+cv2.imshow("test", new_canvas)
+cv2.waitKey(1000)
